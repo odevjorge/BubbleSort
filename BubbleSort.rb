5f7c4de28f9f91afdc109a536d575d05220cb89f
JorgeLuiz <jorge.luiz.g217@gmail.com>
2022-03-13 18:41:23 -0300

    Quase pronto

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,24 +1,41 @@
-# print "Digite uma sequencia de numeros: " 
+# print "Digite uma sequencia de numeros: "
 # lista = gets.chomp.strip.split(" ") #   4	 31	 68	 40	 73	 28	 93	 56	 92	 77
 lista = ["4", "31", "68", "40", "73", "28", "93", "56", "92", "77"]
-ListaSalva = lista
-print lista
+print lista, "\n"
 
-i = 0
 c = 0
+quantasTrocas = 0
 
-while i != 1
-    while c != lista.length - 1
-        print "\n Verificações: #{lista[c]} - #{lista[c+1]}"
-        if lista[c] < lista[c+1]
-            print "\nTroca: #{lista[c]} > #{lista[c+1]}"
-            SalvaNumero = lista[c]
-            lista[c] = 
+loop do
+  print "\n>> reset da quantidade de trocas"
+  c = 0
+  quantasTrocas = 0
+  while c != lista.length - 1
+    numPrimario = lista[c].to_i
+    numSecundario = lista[c + 1].to_i
 
-            print "\n >> Nova ordem: "
-        end
+    print "\nVericando os numeros: #{numPrimario} e #{numSecundario}"
+    if numPrimario > numSecundario
+      print "\n - Troca: #{numPrimario} | #{numSecundario}\n - Para: #{numSecundario} | #{numPrimario}\n"
+      numSalvo = numPrimario
+      lista[c] = numSecundario.to_s
+      lista[c + 1] = numSalvo.to_s
+      quantasTrocas += 1
 
-        c += 1
+    else
+      print "\n - Ok\n"
+      
     end
-    i += 1
+
+    print "\nPressione Qualquer tenta para continuar.."
+    pause = gets
+    c += 1
+  end
+
+  if c == lista.length - 1 && quantasTrocas == 0
+    break
+  end
 end
+
+puts "\nFinal:"
+print lista
--- TestesSeparados.rb
+++ TestesSeparados.rb
@@ -1,9 +1,67 @@
-lista = ["4", "31", "68", "40", "73", "28", "93", "56", "92", "77"]
+lista = ["4", "31", "8", "40", "73", "28", "93", "56", "97", "77"]
 
-print lista
+menorNum = "1"
+i = 0
 
-salvar = lista[0]
-lista[0] = lista[0+1]
-lista[0+1] = salvar
+while i != lista.length
+  if lista[i].length > menorNum.to_s.length
+    menorNum = lista[i]
 
-print "\n#{lista}"
+  end
+  i += 1
+
+end
+
+qntMaior = menorNum.length
+
+i = 0
+while i != lista.length
+  if lista[i].length < qntMaior
+    lista[i] = lista[i].to_s.rjust(qntMaior.to_i, "0")
+
+  end
+  i += 1
+
+end
+
+marcador = "#{("-" * 4) + ("-" * qntMaior)}" * 2
+
+c = 0
+quantasTrocas = 0
+
+loop do
+  c = 0
+  quantasTrocas = 0
+  while c != lista.length - 1
+    numPrimario = lista[c].to_i
+    numSecundario = lista[c + 1].to_i
+
+    print "Verificando:\n", lista, "\n"
+    print " ", "#{(" " * 4) + (" " * qntMaior)}" * c, marcador, "\n"
+
+    if numPrimario > numSecundario
+      print "\n - Troca: #{numPrimario} | #{numSecundario}\n - Para: #{numSecundario} | #{numPrimario}\n"
+      numSalvo = numPrimario
+      lista[c] = numSecundario.to_s.rjust(qntMaior.to_i, "0")
+      lista[c + 1] = numSalvo.to_s.rjust(qntMaior.to_i, "0")
+      quantasTrocas += 1
+
+    else
+      print "\n - Ok\n\n"
+
+    end
+
+    print "\nPrecione qualquer tecla para continuar.."
+    pause = gets
+    print "\e[2J\e[f"
+    c += 1
+  end
+
+  if c == lista.length - 1 && quantasTrocas == 0
+    break
+
+  end
+end
+
+puts "\nFinal:"
+print lista, "\n\n"
